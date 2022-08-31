2c4899403a2ca49087dc26a7cbb742c997ad9e1b
Andrew Myers <andru@cs.cornell.edu>
2022-08-31 08:13:53 -0400

    extend example

--- lectures/encapsulation/src/Main.java
+++ lectures/encapsulation/src/Main.java
@@ -6,8 +6,10 @@ public class Main {
         Rational b = new Rational(1, 3);
         Rational c = Rational.plus(a, b);
         System.out.println(a.toString() + "+" + b + "=" + c);
+            // prints "1/2 + 1/3 = 5/6"
         if (c.equals(new Rational(10, 12))) {
             System.out.println(" = 10/12");
+            // prints "= 10/12"
         }
         /* Rest not in notes */
     }
