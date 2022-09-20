c2f7a746c396f35c6c6a3ad834edff7235f4efa9
Andrew Myers <andru@cs.cornell.edu>
2022-09-19 23:06:13 -0400

    Separate out the semantics of exceptions from the discussion of how to use them

--- lectures/exceptions/arglist.java
+++ lectures/exceptions/arglist.java
@@ -1,14 +1,14 @@
 String filename;
 try {
     for (int i = 0; i < args.length; i++) {
-    switch (args[i]) {
-        case "--file":
-        filename = args[i+1];
-        i++; // advance past filename
-             // as a one-liner: filename = args[++i];
-        break;
-        ...
-    }
+        switch (args[i]) {
+            case "--file":
+            filename = args[i+1];
+            i++; // advance past filename
+                 // as a one-liner: filename = args[++i];
+            break;
+            ...
+        }
     }
 } catch (ArrayIndexOutOfBoundsException e) {
     print_usage_message();
