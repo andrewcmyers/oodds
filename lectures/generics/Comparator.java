5161fbefd7aab6b470a2b0627eb45675f20641eb
Andrew Myers <andru@cs.cornell.edu>
2022-10-04 12:49:10 -0400

    Fix method name swap noted by Siqiu

--- lectures/generics/Comparator.java
+++ lectures/generics/Comparator.java
@@ -2,5 +2,5 @@ interface Comparator<T> {
     /** Compares x and y. Return 0 if x and y are equal, a negative number if x < y, 
      *  and a positive number if x > y.
      */
-    int compareTo(T x, T y);
+    int compare(T x, T y);
 }
--- lectures/trees/comparable.java
+++ lectures/trees/comparable.java
@@ -5,5 +5,5 @@ interface Comparable<T> {
     /** Return 0 if {@code this == y}, a positive number if {@code this > y},
      *  and a negative number if {@code this < y}.
      */
-    int compare(T y);
+    int compareTo(T y);
 }
