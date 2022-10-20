8926770913cde139520f1c7b59e9a67ed2835133
Andrew Myers <andru@cs.cornell.edu>
2022-10-20 14:39:16 -0400

    Simplified quicksort invariant and quicksort code.

--- lectures/sorting/partition.java
+++ lectures/sorting/partition.java
@@ -1,7 +1,8 @@
 /** Partition array {@code a} into {@code a[l..k)} and {@code a[k..r)},
- * where {@code l&lt;k&lt;r}, and all elements in {@code a[l..k)} are
+ * where {@code l < k < r}, and all elements in {@code a[l..k)} are
  * less than or equal to all elements in {@code a[k..r)}.
- * Requires: {@code 0≤l}, {@code r≤a.length}, and {@code r-l≥2}. /
+ * Requires: {@code 0≤l}, {@code r≤a.length}, and {@code r-l≥2}.
+ */
 int partition(int[] a, int l, int r) {
     int p = a[l];
     int i = l-1, j = r;
