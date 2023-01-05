2d2272c34533cae486900bc2e9abd7f46fbf8ed2
Andrew Myers <andru@cs.cornell.edu>
2023-01-05 12:12:05 -0500

    Reorg tree notes.

--- lectures/trees/fisher-yates.java
+++ lectures/trees/fisher-yates.java
@@ -1,5 +1,6 @@
+Random r = new Random(); // the source of randomness
+
 <T> void shuffle(T[] a) {
-   Random r = new Random();
    for (int i = a.length-1; i > 0; i--) {
        int j = r.nextInt(i+1);
        // swap a[j] and a[i]
