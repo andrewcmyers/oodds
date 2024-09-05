ea519106abf342420abf586ce540d98b459b2689
Andrew Myers <andru@cs.cornell.edu>
2024-09-05 09:08:48 -0400

    Use Requires not Checks

--- lectures/subtyping/Board.java
+++ lectures/subtyping/Board.java
@@ -1,9 +1,9 @@
 /** A state of the 2048 game, which contains a 4x4 grid of numeric tiles. */
 interface Board {
-   
    /**
     * Returns: the value of the tile at row r and column c, or 0 if that tile
-    * position is blank. Checks: both r and c are in the range 0..3.
+    *   position is blank.
+    * Requires: both r and c are in the range 0..3.
     */
    int tile(int r, int c);
 
@@ -14,11 +14,10 @@ interface Board {
     * Effect: performs a game move in the specified direction.
     * Returns: true if the move is legal; that is, tiles can be slid in the
     *   specified direction.
-    * Checks: {@code d} is one of 'N', 'S', 'E', or 'W'.
+    * Requires: {@code d} is one of 'N', 'S', 'E', or 'W'.
     */
    boolean move(char d);
 
    /** Effect: Reset the game to the initial state, with all tiles blank. */
    void reset();
-
 }
