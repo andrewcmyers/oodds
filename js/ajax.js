7c9802ec10ec7de11766e9809e546b5892b1e25f
Andrew Myers <andru@cs.cornell.edu>
2024-09-26 21:14:45 -0400

    Switch default view to the ToC view.
    Use the Fetch API instead of old-style XmlHttpRequest stuff.

--- js/ajax.js
+++ js/ajax.js
@@ -1,70 +1,48 @@
-// fetch the contents from the URL "url".  Once successful, apply the function
-// "cont()" to the contents. If unsuccessful, apply the function err(msg).
-function read_from_url(url, cont, err, mimetype) {
-    var undefined;
-    if (mimetype == undefined) mimetype = 'text/xml';
-    let req;
-    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-        req = new XMLHttpRequest();
-        if (req.overrideMimeType)
-            req.overrideMimeType(mimetype);
-    } else if (window.ActiveXObject) { // IE
-        req = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    req.onreadystatechange = function() {
-        if (req.readyState == 4) {
-            if (req.status == 200) {
-                cont(req.responseText);
-            } else {
-                err('Could not read from ' + url + ': Error ' + req.status);
-            }
-        }
-    }
+function fix_url(url) {
     if (!url.match(/^http:/) && !url.match(/^https:/)) {
         let prefix = location.href;
-        url = prefix.replace(/\/[^\/]*$/, '/') + url;
+        return prefix.replace(/\/[^\/]*$/, '/') + url;
     }
-    req.open("GET", url, true);
-    req.send(null);
+    return url
 }
 
-function post_to_url(url, params, cont, err, mimetype) {
-    let undefined;
-    if (mimetype == undefined) mimetype = 'text/xml';
-    let req;
-    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-        req = new XMLHttpRequest();
-        if (req.overrideMimeType)
-            req.overrideMimeType(mimetype);
-    } else if (window.ActiveXObject) { // IE
-        req = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    req.onreadystatechange = function() {
-        if (req.readyState == 4) {
-            if (req.status == 200) {
-                cont(req.responseText)
-            } else {
-                err('Could not read from ' + url + ': Error ' + req.status);
-            }
+async function readEverything(stream) {
+    const reader = stream.getReader()
+    let chunks = []
+    let done, value
+    while (!done) {
+        ({ done, value } = await reader.read())
+        if (value) {
+            chunks.push(new TextDecoder().decode(value))
         }
     }
-    if (!url.match(/^http:/) && !url.match(/^https:/)) {
-        let prefix = location.href;
-        url = prefix.replace(/\/[^\/]*$/, '/') + url;
-    }
+    return chunks.join('')
+}
 
+// fetch the contents from the URL "url".  Once successful, apply the function
+// "cont()" to the contents. If unsuccessful, apply the function err(msg).
+function read_from_url(url, cont, err) {
+    fetch(fix_url(url)).then(
+        response => readEverything(response.body).then(cont),
+        response => readEverything(response.body).then(err))
+}
+
+function post_to_url(url, params, cont, err) {
     let data = new FormData();
     for (var prop in params) {
 	data.append(prop, params[prop]);
     }
 
-    req.open("POST", url, true);
-    req.send(data);
+    fetch(fix_url(url, {
+        method: 'POST',
+        body: data
+    }))
+        .then(cont, err)
 }
 
 // fetch the contents from the URL "url" into DOM node "node".
 // Once successful, apply the optionally provided function "cont()".
-function fetch_content_node(node, url, cont, mimetype) {
+function fetch_content_node(node, url, cont) {
     read_from_url(url,
         responseText => {
             node.innerHTML = responseText;
@@ -73,12 +51,12 @@ function fetch_content_node(node, url, cont, mimetype) {
         },
         errmsg => {
             node.innerHTML = errmsg;
-        }, mimetype);
+        });
 }
 
 // fetch the contents from the URL "url" into the DOM node with id attribute
 // "id".  Once successful, apply the optionally provided function "cont()".
-function fetch_content(id, url, cont, mimetype) {
+function fetch_content(id, url, cont) {
     const node = document.getElementById(id);
-    fetch_content_node(node, url, cont, mimetype);
+    fetch_content_node(node, url, cont);
 }
