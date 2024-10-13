042c3ff2d414aa0ffb2b6fe80d21d25aae0dffdc
Andrew Myers <andru@cs.cornell.edu>
2024-10-12 22:57:23 -0400

    Make more sense to name the element sought x

--- lectures/loopinv/binary_search.java
+++ lectures/loopinv/binary_search.java
@@ -1,12 +1,12 @@
 /**
- * Returns: an index i such that a[i] == k.
- * Requires: k is in a, and a is sorted in ascending order
+ * Returns: an index i such that a[i] equals x.
+ * Requires: x is in a, and a is sorted in ascending order
  */
-int search(int[] a, int k) {
+int search(int[] a, int x) {
     int l = 0, r = a.length-1;
     while (l < r) {
 	int m = (l+r)/2;
-	if (k <= a[m]) r = m;
+	if (x <= a[m]) r = m;
 	else l = m+1;
     }
     return l;
--- lectures/loopinv/binary_search_recursive.java
+++ lectures/loopinv/binary_search_recursive.java
@@ -1,22 +1,22 @@
 /**
- * Returns: an index i such that a[i] == k.
- * Requires: k is in a, and a is sorted in ascending order
+ * Returns: an index i such that a[i] equals x.
+ * Requires: x is in a, and a is sorted in ascending order
  */
-int search(int[] a, int k) {
+int search(int[] a, int x) {
     int l = 0, r = a.length-1;
-    return search_rec(a, k, l, r);
+    return search_rec(a, x, l, r);
 }
 
 /**
- * Returns: an index i such that a[i] == k.
+ * Returns: an index i such that a[i] equals x.
  * Requires: 1. a is sorted in ascending order
  *           2. 0 ≤ l ≤ r < a.length
- *           3. k is in a[l..r]
+ *           3. x is in a[l..r]
  */
-int search_rec(int[] a, int k, int l, int r) {
+int search_rec(int[] a, int x, int l, int r) {
     if (l >= r) return l;
     int m = (l+r)/2;
-    if (k <= a[m]) r = m;
+    if (x <= a[m]) r = m;
     else l = m + 1;
-    return search_rec(a, k, l, r);
+    return search_rec(a, x, l, r);
 }
