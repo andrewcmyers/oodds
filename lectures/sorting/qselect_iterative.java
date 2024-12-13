3c312fb62855db729dbfe782e3725d9d177ce60a
Andrew Myers <andru@cs.cornell.edu>
2024-12-13 18:07:28 -0500

    Tighten code.

--- lectures/sorting/qselect_iterative.java
+++ lectures/sorting/qselect_iterative.java
@@ -3,13 +3,10 @@
  * Requires: {@code 0 ≤ l ≤ n < r ≤ a.length}
  */
 int qselect(int[] a, int l, int r, int n) {
-    while (r-l >= 2) {
+    while (r - l >= 2) {
         int k = partition(a, l, r);
-        if (n < k) {
-            r = k;
-        } else {
-            l = k;
-        }
+        if (n < k) r = k;
+        else l = k;
     }
     return a[l];
 }
